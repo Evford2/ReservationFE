984231d08521ee1a5c6cc6b3c1148e2e77cc5250
Evford2 <evanford28@gmail.com>
2022-11-26 14:28:51 -0600

    added admin page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {
   Route,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Admin from "./pages/admin/Admin";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/admin" element={<Admin/>}/>
       </Routes>
 
     </BrowserRouter>
