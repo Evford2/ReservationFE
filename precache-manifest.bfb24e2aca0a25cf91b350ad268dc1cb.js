66249a6f57e934b8e83f5a3740cea1c0e2126178
Evan Ford <98207065+Evford2@users.noreply.github.com>
2025-09-05 22:49:55 -0500

    Updates

---
+++ precache-manifest.bfb24e2aca0a25cf91b350ad268dc1cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739842d5c924f9bfbde2635487f580b6",
+    "url": "/ReservationFE/index.html"
+  },
+  {
+    "revision": "a0282371ab2c319e206c",
+    "url": "/ReservationFE/static/css/main.3e6e9483.chunk.css"
+  },
+  {
+    "revision": "86d96d1af7eec924bb87",
+    "url": "/ReservationFE/static/js/2.3eb2f410.chunk.js"
+  },
+  {
+    "revision": "c4bd734360e03760b4860e05f599d6fb",
+    "url": "/ReservationFE/static/js/2.3eb2f410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0282371ab2c319e206c",
+    "url": "/ReservationFE/static/js/main.f9f778c2.chunk.js"
+  },
+  {
+    "revision": "8665d59ad02b62553776",
+    "url": "/ReservationFE/static/js/runtime-main.d75f261e.js"
+  },
+  {
+    "revision": "31032358ebae0dbf3e71951bc2585784",
+    "url": "/ReservationFE/static/media/sitemap.31032358.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reservationfe/precache-manifest.c202d103c80379cbe2af33549aae54f5.js"
+  "/ReservationFE/precache-manifest.bfb24e2aca0a25cf91b350ad268dc1cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reservationfe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReservationFE/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
