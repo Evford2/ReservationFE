15c3db495383141c2495d952a2fa994099276262
Evan Ford <98207065+Evford2@users.noreply.github.com>
2025-09-06 00:29:19 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
-import "./style.css"
+import { BrowserRouter } from 'react-router-dom';
+import "./stylRe.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
