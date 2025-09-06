baf1a5c8d830bc4b6790353cc53c2f181da39beb
Evan Ford <98207065+Evford2@users.noreply.github.com>
2025-09-06 00:25:28 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "./style.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/ReservationFE">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
