fab6417fff4b003dba5c2453e91e42504c965f8b
Evan Ford <98207065+Evford2@users.noreply.github.com>
2025-09-12 11:55:05 -0500

    changes

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,27 +2,69 @@ import React from "react";
 import { useRef, useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import "./Register.css";
+import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Login(){
-    const userRef= useRef();
+const EMAIL_REGEX = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+export default function Register(){
+    const emailRef= useRef();
     const errRef = useRef();
 
-    const [user, setUser] = useState('');
+    const [email, setEmail] = useState('');
+    const [validEmail, setValidEmail] = useState(false);
+    const [emailFocus, setEmailFocus] = useState(false);
+
     const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
     const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
 
-    //useEffect(() => {
-        //userRef.current.focus();
-    //}, [])
+    useEffect(() => {
+        emailRef.current.focus();
+    },[])
+
+    useEffect(() => {
+        const result = EMAIL_REGEX.test(email);
+        console.log(result);
+        console.log(email);
+        setValidEmail(result);
+    },[email])
+
+    useEffect(() => {
+        const result = PWD_REGEX.test(pwd);
+        console.log(result);
+        console.log(pwd);
+        setValidPwd(result);
+        const match = pwd === matchPwd;
+        setValidMatch(match);
+    },[pwd, matchPwd])
 
     useEffect(() => {
         setErrMsg('');
-    }, [user, pwd])
+    }, [email, pwd, matchPwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const v1 = EMAIL_REGEX.test(email);
+        const v2 = PWD_REGEX.test(pwd);
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry");
+            return;
+        }
+    }
 
     return(
         <div className="logincontainer">
-        <Form className="loginform">
+        <Form onSubmit={handleSubmit} className="loginform">
 
             <h1 className="loginheader">Log in</h1>
             <Form.Group className="nameform" controlId="formEmail">
@@ -37,8 +79,27 @@ export default function Login(){
             </Form.Group>
             <Form.Group className="emailform" controlId="formEmail">
 
-                <Form.Label className="mb-3">Email address</Form.Label>
-                <Form.Control className="email" type="email" placeholder="Enter email" />
+                <Form.Label className="mb-3">Email address
+                    <span class={validEmail ? "valid" : "hide"}>
+                        <FontAwesomeIcon className="check" icon={faCheck} />
+                    </span>
+                    <span class={validEmail || !email ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                </Form.Label>
+                <Form.Control className="email" type="email" placeholder="Enter email" 
+                     
+                              
+                              controlId="email"
+                              ref={emailRef}
+                              autoComplete="off"
+                              onChange={(e) => setEmail(e.target.value)}
+                              required
+                              aria-invalid={validEmail ? "false" : "true"}
+                              aria-describedby="uidnote"
+                              onFocus={() => setEmailFocus(true)}
+                              onBlur={() => setEmailFocus(false)} 
+                />
 
                 <Form.Label className="mb-3">Password</Form.Label>
                 <Form.Control className="password" type="password" placeholder="Password" />
